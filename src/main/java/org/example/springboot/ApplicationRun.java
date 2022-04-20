63b7c860d1b1802b7fea93696be324a57e11d944
ninggelin <455307376@qq.com>
2022-04-20 17:07:27 +0800

    启动类

---
+++ src/main/java/org/example/springboot/ApplicationRun.java
@@ -0,0 +1,17 @@
+package org.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>启动类</p>
+ *
+ * @author : ninggelin
+ * @date : 2022-04-20 09:37
+ **/
+@SpringBootApplication
+public class ApplicationRun {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRun.class, args);
+    }
+}
