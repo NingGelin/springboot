207a68d0e6fc7fb99d342fe052e9d1a91151000a
ninggelin <455307376@qq.com>
2022-05-09 16:48:42 +0800

    update

---
+++ src/test/java/org/example/springboot/Tmp2.java
@@ -0,0 +1,15 @@
+package org.example.springboot;
+
+/**
+ * <p>临时测试</p>
+ *
+ * @author : ninggelin
+ * @date : 2022-05-06 15:23
+ **/
+public class Tmp2 {
+
+    public static void main(String[] args) {
+        String cpath = "/265477427449859/265477427449860/265477427449861";
+        System.out.println(cpath.split("/")[1]);
+    }
+}
