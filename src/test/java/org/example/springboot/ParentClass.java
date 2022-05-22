22965ba4f3a1b1405289185ceeb409d5e3785e83
ninggelin <455307376@qq.com>
2022-05-22 10:35:44 +0800

    update

---
+++ src/test/java/org/example/springboot/ParentClass.java
@@ -0,0 +1,11 @@
+package org.example.springboot;
+
+public class ParentClass {
+    private int parentX;
+    public ParentClass() {
+        setX(100);
+    }
+    public void setX(int x) {
+        parentX = x;
+    }
+}
---
+++ src/test/java/org/example/springboot/TryInitMain.java
@@ -0,0 +1,8 @@
+package org.example.springboot;
+
+public class TryInitMain {
+    public static void main(String[] args) {
+        ChildClass cc = new ChildClass();
+        cc.printX();
+    }
+}
